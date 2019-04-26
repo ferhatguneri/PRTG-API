f811080c656b6c84a259953e240340db062380e6
Ferhat Güneri <guneriferhat21@gmail.com>
2019-04-26 17:12:14 +0300

    Update PTRG-API.py

--- PTRG-API.py
+++ PTRG-API.py
@@ -1,32 +1,28 @@
-﻿#AddDevice
-def adddevice(devicename,deviceip,devicetempID,probeID):
-# This fonction is inserting device or probe from template to PRTG and resume it to monitoring
-    try:
-        resp = requests.get("https://ServerIPorNAME/api/duplicateobject.htm?id="+str(devicetempID)+"&name="+str(devicename)+"&host="+str(deviceip)+"&targetid="+str(probeID)+"&username=prtgadmin&password=Password",verify=False)
-        print resp
-
-        for i in resp.history:
-            id= i.url.partition("device.htm?id=")
-            deviceid = id[2]
-        respo = requests.get("https://ServerIPorNAME/api/pause.htm?id="+str(deviceid)+"&action=1&username=prtgadmin&password=Password", verify=False)
-        print respo
-    except:
-        print ("Error.")
-#adddevice(devicename,deviceip,devicetempID,probeID)
-
-# Print all probes in PTRG
+#Get All Probes from PRTG
 def getprobes():
-    res = requests.get("https://ServerIPorNAME/api/table.xml?content=probes&output=json&columns=objid,name&filter_parentid=1&username=prtgadmin&password=Password")
+    res = requests.get("https://prtg.server.com/api/table.xml?content=probes&output=json&columns=objid,name&filter_parentid=1&username=prtgadmin&password=*****")
     print res
     res = res.json()
     res = json.dumps(res)
     res = json.loads(res)
     print res
-getprobes()
+#getprobes()
 
-#print all devices in PRTG
+
+# Get Graphic by Sensor ID
+def getgraphic(sensorID):
+    res = requests.get("https://prtg.server.com/api/chartlegend.json?graphid=2&id="+str(sensorID)+"&username=prtgadmin&password=*****")
+    print res
+    res = res.json()
+    res = json.dumps(res)
+    res = json.loads(res)
+    print res
+#getgraphic("1111")
+
+
+#Get all devices from PRTG
 def getdevices():
-    res = requests.get("https://ServerIPorNAME/api/table.xml?content=devices&output=json&columns=objid,probe,group,device,host&username=prtgadmin&password=Password")
+    res = requests.get("https://prtg.server.com/api/table.xml?content=devices&output=json&columns=objid,probe,group,device,host&username=prtgadmin&password=*****")
     print res
     res = res.json()
     res = json.dumps(res)
@@ -34,22 +30,61 @@ def getdevices():
     print res
 #getdevices()
 
-#Print all sensors by device ID
-def getsensors(deviceID):
-    res = requests.get("https://ServerIPorNAME/api/table.xml?content=sensors&output=json&columns=objid,device,sensor,status&id=8297&username=prtgadmin&password=Password")
-    print res
-    res = res.json()
-    res = json.dumps(res)
-    res = json.loads(res)
-    print res
-#getsensors(deviceID)
 
-# get graphic by sensorID for last 2 hour
-def getgraphic(sensorID):
-    res = requests.get("https://ServerIPorNAME/api/chartlegend.json?graphid=2&id="+sensorID+"&username=prtgadmin&password=Password")
+#Get all sensors of device
+def getsensors(deviceID):
+    res = requests.get("https://prtg.server.com/api/table.xml?content=sensors&output=json&columns=objid,device,sensor,status&id="+str(deviceID)+"&username=prtgadmin&password=*****")
     print res
     res = res.json()
     res = json.dumps(res)
     res = json.loads(res)
     print res
-#getgraphic(sensorID)
+#getsensors("1111")
+
+
+#Insert Device - Cloning from template device
+def adddevice(TempDeviceID,DeviceName,DeviceIP,GroupID):
+# Bu komut ile template devide den yeni device oluşturma işlemi yapılır.
+    try:
+        resp = requests.get("https://prtg.server.com/api/duplicateobject.htm?id="+str(TempDeviceID)+"&name="+str(DeviceName)+"&host="+str(DeviceIP)+"&targetid="+str(GroupID)+"&username=prtgadmin&password=*****",verify=False)
+        print resp
+#Bu komutlar ile yeni eklenen cihaz paused eklendiği için aşağıda yeni cihazın id si pars edilerek elde edilir ve cihaz status u resume yapılır.
+        for i in resp.history:
+            id= i.url.partition("device.htm?id=")
+            deviceid = id[2]
+        respo = requests.get("https://prtg.server.com/api/pause.htm?id="+str(deviceid)+"&action=1&username=prtgadmin&password=*****", verify=False)
+        print respo
+    except:
+        print ("Cihaz Eklenirken bir hata oluştu.")
+#adddevice("8480","Ferhat Guneri","1.2.3.4","2037")
+
+
+#Remove Object (Device, Sensor, Probe).
+def removeObject(objectID):
+    res = requests.get("https://prtg.server.com/api/deleteobject.htm?id="+str(objectID)+"&approve=1&username=prtgadmin&password=*****")
+    print res
+#removeObject("8481")
+
+#Pause Object
+def pauseObject(objectID):
+    res = requests.get("https://prtg.server.com/api/pause.htm?id="+str(objectID)+"&action=0&username=prtgadmin&password=*****")
+    print res
+#pauseObject("8330")
+
+#Resume Object
+def resumeObject(objectID):
+    res = requests.get("https://prtg.server.com/api/pause.htm?id="+str(objectID)+"&action=1&username=prtgadmin&password=*****")
+    print res
+resumeObject("2052")
+
+
+#Insert Group - Cloning from Template
+def addGroup(TempGroupID,NewGroupName,DestinationGroupID):
+    res = requests.get("https://prtg.server.com/api/duplicateobject.htm?id="+str(TempGroupID)+"&name="+str(NewGroupName)+"&targetid="+str(DestinationGroupID)+"&username=prtgadmin&password=*****")
+    print res
+    for i in res.history:
+        id= i.url.partition("group.htm?id=")
+        newGroupID = id[2]
+    respo = requests.get("https://prtg.server.com/api/pause.htm?id="+str(newGroupID)+"&action=1&username=prtgadmin&password=*****")
+    print respo
+#addGroup("8508","Yeni Müşteri","1")
